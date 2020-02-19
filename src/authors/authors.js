200aa6b80565de186c26bc191982ed2be079796e
Jacob Ritzhaupt <jacobritzhaupt@gmail.com>
2020-02-19 18:27:10 -0500

    added book cards and started the authors page (#19)

--- src/authors/authors.js
+++ src/authors/authors.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { Component } from "react";
 import "./authors.css";
 
-export default function Home () {
-    return (
-        <div className="Home">
-            <div className="lander">
-                <h1>Authors</h1>
-                <p>Welcome to our selection of authors.</p>
-            </div>
-        </div>
-    );
+export default class Author extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            authors: [
+                {
+                    id: 1,
+                    name: "John Doe"
+                },
+                {
+                    id: 2,
+                    name: "Jane Doe"
+                },
+                {
+                    id: 3,
+                    name: "Billy"
+                }
+            ]
+        };
+    }
 }
--- src/books/books.js
+++ src/books/books.js
@@ -1,13 +1,71 @@
-import React from "react";
+import React, { Component } from "react";
 import "./books.css";
+import { BookDisplay } from "./BookDisplay";
 
-export default function Home () {
-    return (
-        <div className="Home">
-            <div className="lander">
-                <h1>Books</h1>
-                <p>Welcome to our selection of books.</p>
+export default class Books extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            books: [
+                {
+                    id: 1,
+                    title: "Domain Driven Design",
+                    author: "Eric Evans",
+                    isbn: "978-0321125217"
+                },
+                {
+                    id: 2,
+                    title: "Accelerate",
+                    author: "Nicole Forsgren",
+                    isbn: "978-1942788331"
+                },
+                {
+                    id: 3,
+                    title: "Book",
+                    author: "Author",
+                    isbn: "389-9193930"
+                },
+                {
+                    id: 4,
+                    title: "Book",
+                    author: "Author",
+                    isbn: "389-9193930"
+                },
+                {
+                    id: 5,
+                    title: "Book",
+                    author: "Author",
+                    isbn: "389-9193930"
+                },
+                {
+                    id: 6,
+                    title: "Book",
+                    author: "Author",
+                    isbn: "389-9193930"
+                },
+                {
+                    id: 7,
+                    title: "Book",
+                    author: "Author",
+                    isbn: "389-9193930"
+                },
+                {
+                    id: 8,
+                    title: "Book",
+                    author: "Author",
+                    isbn: "389-9193930"
+                }
+            ]
+        };
+    }
+
+    render() {
+        return(
+            <div className="Books">
+                <div className="lander">
+                    <BookDisplay books={this.state.books} />
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
